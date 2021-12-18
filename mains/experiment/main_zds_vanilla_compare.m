6119947a2e7bcc591bc18a944707bfa9f6f8ebc9
Tommaso Buvoli <tommasosignup@gmail.com>
2021-12-17 22:31:16 -0800

    Updated main_zds_vanilla_compare.m -- added low-order integrator test

--- mains/experiment/main_zds_vanilla_compare.m
+++ mains/experiment/main_zds_vanilla_compare.m
@@ -13,7 +13,7 @@ cd(fullfile('../../stepper/equations/', equation))
 init
 cd(wd);
 
-%% == compute reference using RK4 =====================================
+%% == compute reference using RK4 =========================================
 fprintf('Computing reference... ');
 reference_ppars = mergeStructs(struct('rho', 0, 'epsilon', 0), pars, false);
 reference_options = struct('parameters', reference_ppars, 'coeffGenerator', @RK4C);
@@ -23,14 +23,34 @@ RHS = @(t,y,pars) L_ref .* y + NF(t,y,pars);
 fprintf('done.\n');
 
 %% == run integrators =====================================================
+experiment_tag = 'default'; % default (figure 9), third-order, second-order (appendix figures)
 
-runs = {
-    struct('integrator', @ERK,          'legend', 'ERK4',  'options', struct('coeffGenerator', @ERK4, 'parameters', pars), 'linestyle', 's-')
-    struct('integrator', @ESDC,         'legend', 'ESDC6', 'options', struct('tau', lobpts(4, [0 1]), 'm', 5, 'parameters', pars), 'linestyle', 'd-')
-    struct('integrator', @EPBM_PMFCmS,  'legend', 'EPBM5', 'options', struct('z', [-1 ; legpts(4)], 'b', -1 * ones(5), 'mS', 1, 'kappa', 1, 'parameters', pars, 'alpha', 1), 'linestyle', 'o-')
-    struct('integrator', @IMRK,         'legend', 'IMRK4', 'options', struct('coeffGenerator', @IMRK4, 'parameters', pars), 'linestyle', 'k*--')
-    struct('integrator', @(L,N, tspan, y0, Nt, options) rk4(RHS, tspan, y0(:), Nt, options), 'legend', 'RK4', 'options', struct('parameters', pars), 'linestyle', 'p-')
-};
+switch (experiment_tag)
+    case 'second-order' % Appendix Figure
+        runs = {
+            struct('integrator', @ERK,          'legend', 'ERK2',  'options', struct('coeffGenerator', @ERK2, 'parameters', pars), 'linestyle', 's-')
+            struct('integrator', @ESDC,         'legend', 'ESDC2', 'options', struct('tau', lobpts(2, [0 1]), 'm', 2, 'parameters', pars), 'linestyle', 'd-')
+            struct('integrator', @EPBM_PMFCmS,  'legend', 'EPBM2', 'options', struct('z', [-1 ; legpts(1)], 'b', -1 * ones(2), 'mS', 1, 'kappa', 0, 'parameters', pars, 'alpha', 2), 'linestyle', 'o-')
+            struct('integrator', @IMRK,         'legend', 'IMRK2', 'options', struct('coeffGenerator', @IMRK2, 'parameters', pars), 'linestyle', 'k*--')
+            struct('integrator', @(L,N, tspan, y0, Nt, options) rk4(RHS, tspan, y0(:), Nt, options), 'legend', 'RK4', 'options', struct('parameters', pars), 'linestyle', 'p-')
+        };
+    case 'third-order' % Appendix Figure
+        runs = {
+            struct('integrator', @ERK,          'legend', 'ERK3',  'options', struct('coeffGenerator', @ERK3, 'parameters', pars), 'linestyle', 's-')
+            struct('integrator', @ESDC,         'legend', 'ESDC3', 'options', struct('tau', lobpts(3, [0 1]), 'm', 2, 'parameters', pars), 'linestyle', 'd-')
+            struct('integrator', @EPBM_PMFCmS,  'legend', 'EPBM3', 'options', struct('z', [-1 ; legpts(2)], 'b', -1 * ones(3), 'mS', 1, 'kappa', 1, 'parameters', pars, 'alpha', 1), 'linestyle', 'o-')
+            struct('integrator', @IMRK,         'legend', 'IMRK3', 'options', struct('coeffGenerator', @IMRK3, 'parameters', pars), 'linestyle', 'k*--')
+            struct('integrator', @(L,N, tspan, y0, Nt, options) rk4(RHS, tspan, y0(:), Nt, options), 'legend', 'RK4', 'options', struct('parameters', pars), 'linestyle', 'p-')
+        };
+    otherwise % default experiment for generating Figure 2
+        runs = {
+            struct('integrator', @ERK,          'legend', 'ERK4',  'options', struct('coeffGenerator', @ERK4, 'parameters', pars), 'linestyle', 's-')
+            struct('integrator', @ESDC,         'legend', 'ESDC6', 'options', struct('tau', lobpts(4, [0 1]), 'm', 5, 'parameters', pars), 'linestyle', 'd-')
+            struct('integrator', @EPBM_PMFCmS,  'legend', 'EPBM5', 'options', struct('z', [-1 ; legpts(4)], 'b', -1 * ones(5), 'mS', 1, 'kappa', 1, 'parameters', pars, 'alpha', 1), 'linestyle', 'o-')
+            struct('integrator', @IMRK,         'legend', 'IMRK4', 'options', struct('coeffGenerator', @IMRK4, 'parameters', pars), 'linestyle', 'k*--')
+            struct('integrator', @(L,N, tspan, y0, Nt, options) rk4(RHS, tspan, y0(:), Nt, options), 'legend', 'RK4', 'options', struct('parameters', pars), 'linestyle', 'p-')
+        };
+end
 
 num_Nts  = length(Nts);
 num_runs = length(runs);
@@ -51,7 +71,7 @@ parfor i = 1 : num_runs
     end
 end
 
-save(['data-vanilla-', equation, '.mat'], 'runs', 'tspan', 'Nts', 'errors', 'equation')
+save(['data-vanilla-', equation, '-', num2str(experiment_tag), '.mat'], 'runs', 'tspan', 'Nts', 'errors', 'equation')
 
 %% == produce plot =======================================================
 f = figure();
@@ -70,5 +90,5 @@ axis([hs(end) hs(1) 1e-10 1e2])
 hold off;
 set(gca, 'FontSize', 14, 'FontName', 'Minion Pro');
 
-legend(legend_labels, 'Location', 'NorthWest', 'Interpreter', 'latex'); legend boxoff;
-exportFigure(f, struct('SavePath', ['output/vanilla_integrators_', equation], 'Format', 'pdf', 'PaperPosition', [0 0 15 12], 'Renderer', 'opengl'));
+legend(legend_labels, 'Location', 'SouthEast', 'Interpreter', 'latex'); legend boxoff;
+exportFigure(f, struct('SavePath', ['output/vanilla_integrators_', equation, '_', experiment_tag], 'Format', 'pdf', 'PaperPosition', [0 0 15 12], 'Renderer', 'opengl'));
